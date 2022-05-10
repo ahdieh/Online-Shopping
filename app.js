61d02c97667b85f5c9796ed7f1746c2e712b24b8
ahdiehemadi <ahdieh_e@yahoo.com>
2022-05-10 16:52:46 -0400

    Create a NodeJS application

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>This is a NodeJS application.</h1>');
+});
+
+server.listen(3000);
